23e014501a63120ffb721bb71061fec91b0efe8f
RedPlanetTesting <anjani.veggalam@cigniti.com>
2015-12-15 06:28:17 +0530

    re arranged tests

--- RedPlanetTest/src/test/java/com/mobile/scripts/RP_006_BookingAsAMember.java
+++ RedPlanetTest/src/test/java/com/mobile/scripts/RP_006_BookingAsAMember.java
@@ -106,7 +106,7 @@ public class RP_006_BookingAsAMember extends LoginHelper{
   	@DataProvider(name="testData")
 	public Object[][] createdata1() {
   		return (Object[][]) new Object[][] {
-  			/*{xlsBook.getCellValue("ValidCredentials", "Value"),xlsBook.getCellValue("ValidCredentials", "password"),
+  			{xlsBook.getCellValue("ValidCredentials", "Value"),xlsBook.getCellValue("ValidCredentials", "password"),
 		  		xlsBook.getCellValue("country", "Value"),xlsBook.getCellValue("city", "Value"),
 				xlsBook.getCellValue("fName", "Value"),xlsBook.getCellValue("lName", "Value"),
 				xlsBook.getCellValue("email", "Value"),"",
@@ -119,7 +119,7 @@ public class RP_006_BookingAsAMember extends LoginHelper{
 			  		xlsBook.getCellValue("email", "Value"),xlsBook.getCellValue("invalidCardHolder", "Value"),
 			  		xlsBook.getCellValue("invalidCardNum", "Value"),xlsBook.getCellValue("expirationmonth", "Value"),
 			  		xlsBook.getCellValue("expirationyear", "Value"),xlsBook.getCellValue("invalidCVV", "Value"),
-			  		false,"Validate Hotel Booking as a Guest with invalid payment details"},*/
+			  		false,"Validate Hotel Booking as a Guest with invalid payment details"},
   			{xlsBook.getCellValue("ValidCredentials", "Value"),xlsBook.getCellValue("ValidCredentials", "password"),
   				xlsBook.getCellValue("country", "Value"),xlsBook.getCellValue("city", "Value"),
   				xlsBook.getCellValue("fName", "Value"),xlsBook.getCellValue("lName", "Value"),
