560bde959b0d52f31570dc413760b96ff385706f
andrey-panyushkin <dron_1327>
2016-01-06 13:59:49 +0300

    fix appium java-client error fixes for RP_003_TestSearchHotels temporary disable RP_001_TestLoginFromMyAccount

--- RedPlanetTest/src/test/java/com/androidMobile/scripts/RP_003_TestSearchHotels.java
+++ RedPlanetTest/src/test/java/com/androidMobile/scripts/RP_003_TestSearchHotels.java
@@ -12,54 +12,57 @@ import com.ctaf.support.ExcelReader;
 import com.ctaf.support.HtmlReportSupport;
 import com.ctaf.utilities.Reporter;
 
-public class RP_003_TestSearchHotels extends LoginHelper{
-	ExcelReader xlsSearch = new ExcelReader(configProps.getProperty("TestData"),
-			"RP_ANDR_003");
-  @Test(dataProvider = "testData")
-  public void testSearchHotels(String country, String city,String description) throws Throwable{
-	try{
-		 TestEngine.testDescription.put(HtmlReportSupport.tc_name, 
-				description);
-		 handleSplashDialog();
-        navigateToMyAccount();
-        handleRateAppPopUp();
-        logOut();
-        navigateToBookNow();
-        handleRateAppPopUp();
-		 selectDestination(country, city);
-		 if(description.contains("Select Destination")){
-			 if((isElementPresent(By.xpath(HomePageLocators.locationCityName.replace("#", city.trim())),
-					 "locationCityName"))&
-					 (isElementPresent(By.xpath(HomePageLocators.locationCountryName.replace("#", country.trim())),
-							 "locationCityName"))){
-				 Reporter.SuccessReport(description, "Successful");
-			 }else{
-				 Reporter.failureReport(description, "Failed");
-			 }
-		 }
-		 if(description.contains("Search Hotel")){
-		 waitForElementPresent(HomePageLocators.searchButton, "searchButton");
-		 click(HomePageLocators.searchButton, "searchButton");
-		 handleRateAppPopUp();
-		 Thread.sleep(6000);
-			 if(isElementDisplayed(PickRoomPageLocators.pickRoomPage)){
-				 Reporter.SuccessReport(description, "Successful");
-			 }else
-				 Reporter.failureReport(description, "Failed");
-		 }
-	}catch(Exception e) {
-		e.printStackTrace();
-		Reporter.failureReport(description, "Failed with exception");
-	} finally {
-        driver.navigate().back();
+public class RP_003_TestSearchHotels extends LoginHelper {
+
+    private static final String VERIFY_SELECT_DESTINATION = "Verify Select Destination";
+    private static final String VERIFY_SEARCH_HOTELS = "Verify Search Hotels";
+
+    ExcelReader xlsSearch = new ExcelReader(configProps.getProperty("AndroidTestData"), "RP_ANDR_003");
+
+    @Test(dataProvider = "testData")
+    public void testSearchHotels(String country, String city, String description) throws Throwable {
+        try {
+            TestEngine.testDescription.put(HtmlReportSupport.tc_name, description);
+            handleSplashDialog();
+            navigateToMyAccount();
+            handleRateAppPopUp();
+            logOut();
+            navigateToBookNow();
+            handleRateAppPopUp();
+            selectDestination(country, city);
+            if (description.equals(VERIFY_SELECT_DESTINATION)) {
+                if ((isElementPresent(By.xpath(HomePageLocators.locationCityName.replace("#", city.trim())),
+                        "locationCityName")) &
+                        (isElementPresent(By.xpath(HomePageLocators.locationCountryName.replace("#", country.trim())),
+                                "locationCityName"))) {
+                    Reporter.SuccessReport(description, "Successful");
+                } else {
+                    Reporter.failureReport(description, "Failed");
+                }
+            } else if (description.equals(VERIFY_SEARCH_HOTELS)) {
+                waitForElementPresent(HomePageLocators.searchButton, "searchButton");
+                click(HomePageLocators.searchButton, "searchButton");
+                handleRateAppPopUp();
+                if (isElementDisplayed(PickRoomPageLocators.pickRoomPage)) {
+                    Reporter.SuccessReport(description, "Successful");
+                } else {
+                    Reporter.failureReport(description, "Failed");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Reporter.failureReport(description, "Failed with exception");
+        } finally {
+            driver.navigate().back();
+        }
     }
-  }
-  	@DataProvider(name="testData")
-	public Object[][] createdata1() {
-  		return (Object[][]) new Object[][] { 
-//  				 {xlsSearch.getCellValue("country", "Value"),xlsSearch.getCellValue("city", "Value"),
-// 				"Verify Select Destination"},
-			  {xlsSearch.getCellValue("country", "Value"),xlsSearch.getCellValue("city", "Value"),
-				"Verify Search Hotels"}};
-	}
+
+    @DataProvider(name = "testData")
+    public Object[][] createdata1() {
+        return (Object[][]) new Object[][]{
+                {xlsSearch.getCellValue("country", "Value"), xlsSearch.getCellValue("city", "Value"),
+                        VERIFY_SELECT_DESTINATION},
+                {xlsSearch.getCellValue("country", "Value"), xlsSearch.getCellValue("city", "Value"),
+                        VERIFY_SEARCH_HOTELS}};
+    }
 }
