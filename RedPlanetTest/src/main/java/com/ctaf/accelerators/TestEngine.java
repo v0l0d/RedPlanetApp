4c497d9de95485caae3097729820442e7f460ebe
andrey-panyushkin <dron_1327>
2016-01-11 13:00:53 +0300

    created result folder with jenkins build number with onr test to check

--- RedPlanetTest/src/main/java/com/ctaf/accelerators/TestEngine.java
+++ RedPlanetTest/src/main/java/com/ctaf/accelerators/TestEngine.java
@@ -40,7 +40,7 @@ public class TestEngine extends HtmlReportSupport {
 			configProps.getProperty("counterPath"));
 	public static String currentSuite = "";
 	public static String method = "";
-	public static String timeStamp = ReportStampSupport.timeStamp()
+	public static String timeStamp = System.getenv("BUILD_NUMBER") != null ? System.getenv("BUILD_NUMBER") : ReportStampSupport.timeStamp()
 			.replace(" ", "_").replace(":", "_").replace(".", "_");
 	public static boolean flag = false;
 	
@@ -229,9 +229,8 @@ public class TestEngine extends HtmlReportSupport {
 				DesiredCapabilities capabilitiesForAppium = new DesiredCapabilities();
 				System.out.println("DeviceName is : " + DeviceName);
                 String androidApkPath = new File(System.getProperty("user.dir"), configProps.getProperty("androidApkPath")).toString();
-                System.out.println("!!!!!!!!!!!!!!!!!!+++++++++++ checking for existing " + androidApkPath);
                 if (new File(androidApkPath).exists()) {
-                    System.out.println("!!!!!!!!!!!!!!!!!! file exists " + androidApkPath);
+                    System.out.println("!!!!!!!!!!!!!!!!!! installing apk to device" + androidApkPath);
                     capabilitiesForAppium.setCapability("app", androidApkPath);
                 }
                 capabilitiesForAppium.setCapability("nativeInstrumentsLib", true);
